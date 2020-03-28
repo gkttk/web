d2bef68885b6be210b7d9137bc8b6eb94b2b7815
Kirill Sviridov <gkttk@mail.ru>
2020-03-28 18:15:07 +0300

    fixSomeBug + made tests branch_6

--- web/src/main/java/HelloUserServlet.java
+++ web/src/main/java/HelloUserServlet.java
@@ -7,16 +7,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 import java.io.IOException;
-import java.io.PrintWriter;
+
 
 @WebServlet(name = "HelloUserServlet", urlPatterns = "/helloUser")
 public class HelloUserServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         response.setContentType("text/html");
-
 
       /*  HttpSession session = request.getSession();
         User userAuth = (User)session.getAttribute("userAuth");*/
@@ -25,19 +23,13 @@ public class HelloUserServlet extends HttpServlet {
             User user = new User(request.getParameter("login"), request.getParameter("password"));
             session.setAttribute("userAuth", user);
         }*/
+
+
         HttpSession session = request.getSession();
-        request.setAttribute("nameAuthUser", request.getParameter("login"));
+        session.setAttribute("user", request.getParameter("login"));
+        session.setAttribute("list", UserService.getUsers().keySet());
+        getServletContext().getRequestDispatcher("/wage.jsp").forward(request,response);
 
-        request.setAttribute("user", request.getParameter("login"));
-        request.setAttribute("list", UserService.getUsers().keySet());
-      getServletContext().getRequestDispatcher("/wage.jsp").forward(request,response);
-
-
-      /*  PrintWriter printWriter = response.getWriter();
-        printWriter.println("<h1> Hello, " + request.getParameter("login") + "!</h1>");
-        printWriter.println("<h1> Registered users: </h1>");
-
-        UserService.getUsers().keySet().stream().forEach(user -> printWriter.println(user));*/
 
     }
 
